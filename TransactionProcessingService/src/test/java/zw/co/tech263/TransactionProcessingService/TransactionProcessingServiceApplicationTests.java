f8cecebe9aebf39e297ec12e97438ab3f569233f
Dereck Chmaboko <dchamboko@gmail.com>
2023-11-23 16:29:23 +0200

    Add transaction processing microservice

---
+++ TransactionProcessingService/src/test/java/zw/co/tech263/TransactionProcessingService/TransactionProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.tech263.TransactionProcessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
