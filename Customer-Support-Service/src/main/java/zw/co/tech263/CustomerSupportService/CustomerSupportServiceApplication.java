7bc022aa293e8e6d93b543343c218f9d10ad017b
Dereck Chmaboko <dchamboko@gmail.com>
2023-11-27 16:41:44 +0200

    refactoring

--- Customer-Support-Service/src/main/java/zw/co/tech263/CustomerSupportService/CustomerSupportServiceApplication.java
+++ Customer-Support-Service/src/main/java/zw/co/tech263/CustomerSupportService/CustomerSupportServiceApplication.java
@@ -2,9 +2,14 @@ package zw.co.tech263.CustomerSupportService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoTransactionManager;
 
 @SpringBootApplication
 public class CustomerSupportServiceApplication {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerSupportServiceApplication.class, args);
